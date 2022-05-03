87246236c6196902851723028ee72731381d6f03
Maple <maple.zhu7@gmail.com>
2022-05-03 16:08:49 -0700

    Section 22, an EJS proj for a blog website

--- Node/ejs-challenge/app.js
+++ Node/ejs-challenge/app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,15 +18,43 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function (req, res){
+    res.render("home", {
+        home: homeStartingContent,
+        posts: posts,
+        _:_});
+})
 
+app.get("/about", function (req,res){
+    res.render("about", {about: aboutContent});
+})
 
+app.get("/contact", function (req,res){
+    res.render("contact", {contact: aboutContent});
+})
 
+app.get("/compose", function (req,res){
+    res.render("compose", {});
+})
 
+app.get("/posts/:t", function (req, res){
+    posts.forEach(function (p){
+        if(_.lowerCase(p.title) == _.lowerCase(req.params.t)){
+            res.render("post", {postTitle: p.title, postBody: p.body})
+        }
+    })
 
+})
 
+app.post("/compose", function (req,res){
+    const post = {
+        title: req.body.postTitle,
+        body: req.body.postBody
+    }
 
-
-
+    posts.push(post);
+    res.redirect("/");
+})
 
 
 
