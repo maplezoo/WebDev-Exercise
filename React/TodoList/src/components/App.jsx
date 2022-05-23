2a4d5e309fdd2d42d636c4727c071b73c34d2c5e
Maple <maple.zhu7@gmail.com>
2022-05-23 16:13:43 -0700

    seperate TodoItem from App.jsx

--- React/TodoList/src/components/App.jsx
+++ React/TodoList/src/components/App.jsx
@@ -1,41 +1,50 @@
 import React, { useState } from "react";
+import TodoItem from "./TodoItem";
 
 function App() {
-  const [todo, setTodo] = useState(["A Item", "Second"]);
-  const [input, setInput] = useState("");
+    const [todo, setTodo] = useState(["A Item", "Second"]);
+    const [input, setInput] = useState("");
 
-  function logTyping(e) {
-    const newTodo = e.target.value;
-    setInput(newTodo);
-  }
+    function logTyping(e) {
+        const newTodo = e.target.value;
+        setInput(newTodo);
+    }
 
-  function addTodo() {
-    setTodo((p) => {
-      return [...p, input];
-    });
-    setInput("");
-  }
+    function addTodo() {
+        setTodo((p) => {
+            return [...p, input];
+        });
+        setInput("");
+    }
 
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input onChange={logTyping} type="text" value={input} />
-        <button onClick={addTodo}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {todo.map((e, idx) => (
-            <li key={idx}>{e}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    function deleteItem(id) {
+        setTodo((p) => {
+            return p.filter((item, idx) => {
+                return idx !== id;
+            });
+        });
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input onChange={logTyping} type="text" value={input} />
+                <button onClick={addTodo}>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {todo.map((e, idx) => (
+                        <TodoItem text={e} onChecked={deleteItem} id={idx} />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
